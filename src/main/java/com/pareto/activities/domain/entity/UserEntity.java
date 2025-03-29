1ec78a36126c6188b78d87e59de216028bc521e9
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-03-29 23:44:42 +0400

    added UserEntity and UserRepository

---
+++ src/main/java/com/pareto/activities/domain/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package com.pareto.activities.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private EParticipantCategory role;
+}
