8933fc1e8d4c54368b63114b97a4632e0e7fe310
eyvaz.bayramov <eyvaz.bayramov2018@gmail.com>
2025-03-31 21:45:38 +0400

    use jetty instead of tomcat

--- src/main/java/com/pareto/activities/mapper/UserMapper.java
+++ src/main/java/com/pareto/activities/mapper/UserMapper.java
@@ -3,10 +3,7 @@ package com.pareto.activities.mapper;
 import com.pareto.activities.DTO.UserCreateRequest;
 import com.pareto.activities.DTO.UserResponse;
 import com.pareto.activities.entity.UserEntity;
-import org.apache.catalina.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
