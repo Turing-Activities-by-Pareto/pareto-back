40af52aa89caf625fcd8f589f3de21a6534a77bd
eyvaz.bayramov <eyvaz.bayramov2018@gmail.com>
2025-04-01 15:44:12 +0400

    Fix redis environment variables.

--- src/main/java/com/pareto/activities/redis/config/RedisConfig.java
+++ src/main/java/com/pareto/activities/redis/config/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.pareto.activities.redis.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,7 +12,17 @@ import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 @RequiredArgsConstructor
+@ConfigurationProperties(prefix = "redis")
 public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private String redisPort;
+
+    @Value("${redis.password}")
+    private String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -20,6 +32,9 @@ public class RedisConfig {
         poolConfig.setMinIdle(2);
 
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(redisHost);
+        jedisConnectionFactory.setPort(Integer.parseInt(redisPort));
+        jedisConnectionFactory.setPassword(redisPassword);
         jedisConnectionFactory.setPoolConfig(poolConfig);
         return jedisConnectionFactory;
     }
